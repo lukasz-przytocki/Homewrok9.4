d698f6ea806c6ee1660598c15fb50a71e2aa9bc4
lukasz.przytocki <lukasz.przytocki@gmail.com>
2019-05-30 19:20:56 +0200

    Added overloaded method with matrix array.

--- src/ArrayComparator.java
+++ src/ArrayComparator.java
@@ -4,7 +4,6 @@ public class ArrayComparator {
         boolean isEqual=true;
         int i=0;
 
-
         if(table1==null || table1.length==0 || table2==null || table2.length==0 || table1.length!=table2.length){
             isEqual = false;
 
@@ -19,4 +18,32 @@ public class ArrayComparator {
         }
         return isEqual;
     }
+
+
+    boolean compare(int[][] table1, int[][] table2) {
+        boolean isEqual = true;
+        int i = 0;
+
+
+        if (table1 == null || table1.length == 0 || table2 == null || table2.length == 0 || table1.length != table2.length) {
+            isEqual = false;
+
+        } else {
+
+            while (isEqual && i < table1.length) { //assumption square matrix
+                int j = 0;
+                while (isEqual && j < table1.length) {
+                    if (table1[i][j] != table2[i][j]) {
+                        isEqual = false;
+                    }
+                    j++;
+                }
+                i++;
+            }
+
+        }
+        return isEqual;
+    }
 }
+
+
--- src/ArrayTest.java
+++ src/ArrayTest.java
@@ -1,12 +1,22 @@
 public class ArrayTest {
     public static void main(String[] args) {
         ArrayComparator arrayComparator = new ArrayComparator();
-        int[] table1={1,2,3,4};
+        int[] table0={1,2,3,4};
+        int[] table1={1,2,3};
         int[] table2={1,2,3};
-        int[] table3={1,8,3};
+        int[] table3={1,9,3};
 
+        int[][] table44 = {{1,2},{3,4}};
+        int[][] table55 = {{1,2},{3,4}};
+        int[][] table66 = {{1,2},{2,4}};
+
+        System.out.println("Tables are even?: " + arrayComparator.compare(table0, table1));
         System.out.println("Tables are even?: " + arrayComparator.compare(table1, table2));
         System.out.println("Tables are even?: " + arrayComparator.compare(table2, table3));
+
+        System.out.println("Tables are even?: " + arrayComparator.compare(table44, table55));
+        System.out.println("Tables are even?: " + arrayComparator.compare(table55, table66));
+
 
 
     }
